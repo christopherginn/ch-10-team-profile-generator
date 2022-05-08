b3372de7e4068cad79e84ae4cf49ce294a2087df
christopherginn <hey.im.chris@live.com>
2022-05-08 16:59:29 -0400

    Figured out Employee class

--- index.js
+++ index.js
@@ -1,18 +1,49 @@
 const inquirer = require('inquirer');
+const fs = require('fs');
 const Employee = require('./lib/Employee')
 
-inquirer
-    .prompt([
-        {
-            type: 'input',
-            message: 'Name?',
-            name: 'confirm'
-        }
-    ])
-    .then((response) => {
-        if (response.confirm === 'yes'){
-            console.log("Yay!")
-        } else {
-            console.log("Seems like it does...")
-        }
-    })
+const team = [];
+
+function addEmployee() {
+    inquirer
+        .prompt([
+            {
+                type: 'input',
+                message: 'Name?',
+                name: 'name'
+            },
+            {
+                type: 'input',
+                message: 'ID?',
+                name: 'id'
+            },
+            {
+                type: 'input',
+                message: 'email?',
+                name: 'email'
+            },
+            {
+                type: 'list',
+                message: 'Do you have any more employees to add?',
+                name: 'addMore',
+                choices: [
+                    {name: 'Yes', value: 'yes'},
+                    {name: 'No', value: 'no'}
+                ]
+            }
+        ])
+        .then((response) => {
+            team.push(new Employee(response.name, response.id, response.email))
+            if (response.addMore === 'yes'){
+                addEmployee();
+            } else {
+                console.log(team)
+            }
+        })
+};
+
+function init (){
+    addEmployee();
+}
+
+init();
