f7d36aae1205c1ad5cbe597869e065c045b44c08
christopherginn <hey.im.chris@live.com>
2022-05-10 17:54:26 -0400

    Most of HTML mark up plugged in. Need to tweak styling and remove unwanted characters

--- src/employees.js
+++ src/employees.js
@@ -64,7 +64,37 @@ function createEmployeeCards(employees) {
 }
 
 function render(employees){
-    return `${createEmployeeCards(employees)}`
+    return `
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+      <meta charset="UTF-8" />
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+      <title>Employees</title>
+      <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+      <link rel="stylesheet" href="style.css">
+      <script src="https://kit.fontawesome.com/c502137733.js"></script>
+    </head>
+    <body>
+      <div class="container-fluid">
+          <div class="row">
+              <div class="col-12 jumbotron mb-3 team-header">
+                  <h1 class="text-center">My Team</h1>
+              </div>
+          </div>
+      </div>
+      <div class="container">
+          <div class="row">
+              <div class="employee-main col-12 d-flex justify-content-center">
+                  ${createEmployeeCards(employees)}
+              </div>
+          </div>
+      </div>
+    </body>
+    </html>
+    
+    `
 }
 
 module.exports = render;
